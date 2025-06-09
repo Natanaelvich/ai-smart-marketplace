4821c7d53c0ee485d6d4b7263beb3b6dadc560fd
Natanael <taelima1997@gmail.com>
2025-06-09 17:49:53 -0300

    Add /response endpoint to app.ts for generating a shopping cart based on user input. Integrates generateCart function from openai module to enhance product recommendation features.

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,10 @@
 import express from 'express';
-import { embedProducts, generateEmbedding, generateProducts } from './openai';
+import {
+  embedProducts,
+  generateCart,
+  generateEmbedding,
+  generateProducts,
+} from './openai';
 import { produtosSimilares, todosProdutos } from './database';
 
 const app = express();
@@ -36,6 +41,14 @@ app.post('/embedding', async (req, res) => {
   const { input } = req.body;
   await generateEmbedding(input);
   res.status(201).end();
+});
+
+app.post('/response', async (req, res) => {
+  const { input } = req.body;
+
+  const cart = await generateCart(input, ['feijão', 'detergente']);
+
+  res.json(cart);
 });
 
 export default app;
