0104a1bc2f1b9b9bb096fc0ad29b6c5ed97198d2
Natanael <taelima1997@gmail.com>
2025-07-13 12:07:39 -0300

    Integrate CartModule into app.module.ts
    Added CartModule to the imports array in app.module.ts to enable shopping cart functionality within the application. This change supports the recent implementation of cart management features.

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { CatalogModule } from './catalog/catalog.module';
+import { CartModule } from './cart/cart.module';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [CatalogModule, ConfigModule.forRoot({ isGlobal: true })],
+  imports: [
+    CatalogModule,
+    CartModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+  ],
   controllers: [],
   providers: [],
 })
