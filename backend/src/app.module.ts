a37161c54e9911c419c3923799a5788946f501cc
Natanael <taelima1997@gmail.com>
2025-07-14 17:34:44 -0300

    Add Chat module to app.module.ts for chat session management

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { CatalogModule } from './catalog/catalog.module';
 import { CartModule } from './cart/cart.module';
 import { ConfigModule } from '@nestjs/config';
+import { ChatModule } from './chat/chat.module';
 
 @Module({
   imports: [
     CatalogModule,
     CartModule,
     ConfigModule.forRoot({ isGlobal: true }),
+    ChatModule,
   ],
   controllers: [],
   providers: [],
