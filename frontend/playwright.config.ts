167b0fa903848a50026f2fe6c90451d470286422
Natanael <taelima1997@gmail.com>
2025-07-16 16:06:22 -0300

    Add Playwright for end-to-end testing
    - Integrated Playwright as a development dependency for end-to-end testing.
    - Added a new Playwright configuration file to define test settings and browser options.
    - Updated package.json to include a test command for running Playwright tests.
    - Modified .gitignore to exclude Playwright test results from version control.

---
+++ frontend/playwright.config.ts
@@ -0,0 +1,28 @@
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './tests',
+  timeout: 30 * 1000,
+  retries: 0,
+  workers: 1,
+  use: {
+    baseURL: 'http://localhost:3001',
+    screenshot: 'on', // tira print em todo teste
+    video: 'retain-on-failure',
+    trace: 'on-first-retry',
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    // },
+  ],
+}); 
